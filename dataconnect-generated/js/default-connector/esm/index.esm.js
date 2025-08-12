9e4e68a641bba064e13c0c8616ccdf8dce5632b1
Kavya Chauhan <142385565+kavya020805@users.noreply.github.com>
2025-08-13 01:01:27 +0530

    Delete dataconnect-generated/js/default-connector directory

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'project1',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'project1',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
